93e008145515f3bc5de7ee0e9849ed160348c088
Francesco Brivio <francesco.brivio@cern.ch>
2016-10-09 12:36:17 +0200

    adding BDT reader for mutau channel

--- Analysis/source/bbmutauAnalyzer.cxx
+++ Analysis/source/bbmutauAnalyzer.cxx
@@ -2,6 +2,7 @@
 This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 
 #include "Analysis/include/SemileptonicEventAnalyzer.h"
+#include "Analysis/include/BDT_reader_mutau.h" /*Francesco*/
 
 namespace analysis {
 
@@ -18,17 +19,22 @@ protected:
 
         const MuonCandidate& muon = event.GetFirstLeg();
         const TauCandidate& tau = event.GetSecondLeg();
+		double BDT_wp = 0.0; /*Francesco*/
+		double BDT_output = MVA_reader.BDT_score(event);
+		//std::cout<<"  BDT score: " << BDT_output << " passed: " << (BDT_output > BDT_wp) <<std::endl;
 
         if(tau->againstMuon3(DiscriminatorWP::Tight) < tauID::againstMuonTight3
                 || tau->againstElectronMVA6(DiscriminatorWP::VLoose) < tauID::againstElectronVLooseMVA6
-                || muon->iso() >= 0.15
-                || event->extraelec_veto || event->extramuon_veto)
+                || muon->iso() >= muonID::pFRelIso
+                || event->dilepton_veto
+				|| BDT_output < BDT_wp /*Francesco*/
+                /*|| (event.extraelec_veto || event.extramuon_veto) */)
             return EventRegion::Unknown;
 
         const bool os = muon.GetCharge() * tau.GetCharge() == -1;
-//        const bool iso = event.byTightIsolationMVArun2v1DBoldDMwLT_2 > 0.5;
+		//const bool iso = event.byTightIsolationMVArun2v1DBoldDMwLT_2 > 0.5;
         const bool iso = tau->iso() > 0.2;
-        //        const bool low_mt = event.pfmt_1 < muonID::mt;
+        //const bool low_mt = event.pfmt_1 < muonID::mt;
         const bool low_mt = true;
 
         if(iso && os) return low_mt ? EventRegion::OS_Isolated : EventRegion::OS_Iso_HighMt;
@@ -36,6 +42,10 @@ protected:
         if(os) return low_mt ? EventRegion::OS_AntiIsolated : EventRegion::OS_AntiIso_HighMt;
         return low_mt ? EventRegion::SS_AntiIsolated : EventRegion::SS_AntiIso_HighMt;
     }
+	
+private:
+	BDT_reader MVA_reader;
+	
 };
 
 } // namespace analysis
