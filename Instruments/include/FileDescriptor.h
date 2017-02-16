bb5361b7f479f1e60301bdd876f4f85cda46c068
Maria Teresa <grippomariateresa@gmail.com>
2017-02-16 14:38:57 +0100

    Cleaned code and implemented first version of weight calculation - not working

--- Instruments/include/FileDescriptor.h
+++ Instruments/include/FileDescriptor.h
@@ -9,6 +9,7 @@ This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 #include "AnalysisTools/Core/include/EnumNameMap.h"
 #include "AnalysisTools/Core/include/NumericPrimitives.h"
 #include "AnalysisTools/Core/include/PhysicalValue.h"
+#include "h-tautau/Analysis/include/SummaryTuple.h"
 
 namespace analysis {
 
@@ -29,10 +30,18 @@ struct DYBinDescriptor {
     PhysicalValue nu;
     PhysicalValue weight;
     std::string ref_sample;
+    FileType ref_fileType;
 
     DYBinDescriptor()
         : n_jet(0,0), n_bjet(0,0),n_ht(0,0),nu(0.0, std::numeric_limits<double>::infinity()),
           weight(std::numeric_limits<double>::quiet_NaN()) {}
+
+    bool Contains(const ntuple::GenId& genId) const
+    {
+        return n_jet.Contains(genId.n_partons) &&
+              n_bjet.Contains(genId.n_b_partons) &&
+              n_ht.Contains(genId.ht10_bin);
+    }
 
     static std::vector<DYBinDescriptor> LoadConfig(const std::string& config_file)
     {
@@ -51,6 +60,29 @@ struct DYBinDescriptor {
             dyBinDescriptors.push_back(descriptor);
         }
         return dyBinDescriptors;
+    }
+
+    static std::ofstream SaveCfg(const std::string& output_file, const std::vector<DYBinDescriptor>& output_bins)
+    {
+        std::ofstream cfg(output_file);
+        if(cfg.fail())
+            throw analysis::exception("Unable to create outputfile'");
+        cfg.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+
+        cfg << "#n_jet_min n_jet_max n_bjet_min n_bjet_max ht_bin_min ht_bin_max weight rel_err_w "
+               "nu rel_err_nu ref_sample\n";
+
+        for(unsigned i = 0; i < output_bins.size(); ++i)
+        {
+            DYBinDescriptor output_bin = output_bins.at(i);
+            cfg << output_bin.n_jet.min() << " " <<output_bin.n_jet.max()  << " " <<
+                   output_bin.n_bjet.min() << " " << output_bin.n_bjet.max() << " " <<
+                   output_bin.n_ht.min() << " " << output_bin.n_ht.max() << " " <<
+                   output_bin.weight.GetValue() << " " << output_bin.weight.GetRelativeStatisticalError() << " " <<
+                   output_bin.nu.GetValue() << " " << output_bin.nu.GetRelativeStatisticalError() <<
+                " " << output_bin.ref_sample <<  "\n";
+        }
+        return cfg;
     }
 };
 
--- Instruments/source/FileMerger.cxx
+++ Instruments/source/FileMerger.cxx
@@ -2,49 +2,45 @@
 This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 #include <boost/format.hpp>
 #include "AnalysisTools/Run/include/program_main.h"
-#include "AnalysisTools/Core/include/ConfigReader.h" // definition of wrappers for the program main and program arguments.
+#include "AnalysisTools/Core/include/ConfigReader.h"
 #include "AnalysisTools/Core/include/RootExt.h"
 #include "h-tautau/Analysis/include/EventInfo.h"
 #include "Instruments/include/FileConfigEntryReader.h"
 #include "AnalysisTools/Core/include/NumericPrimitives.h"
 
-struct Arguments { // list of all program arguments
-    //REQ_ARG(std::string, input_file); // required argument "input_file"
-    REQ_ARG(std::string, tree_name); // tree name
-    REQ_ARG(std::string, cfg_name); // cfg name
-    REQ_ARG(std::string, file_cfg_name); // DY cfg name
-    REQ_ARG(std::string, output_file); // output file
+
+//std::string tree_name{"tree_name", "Tree on which we work"};
+struct Arguments {
+    REQ_ARG(std::string, tree_name);
+    REQ_ARG(std::string, cfg_name);
+    REQ_ARG(std::string, file_cfg_name);
+    REQ_ARG(std::string, output_file);
 };
 
 namespace analysis {
 
+
+template<typename BinDescriptor, typename GenMap>
 struct SampleDescriptor {
-    DYBinDescriptor bin;
-    using GenMap = ntuple::GenEventCountMap;
+    BinDescriptor bin;
     GenMap gen_counts;
 
     size_t Integral() const
     {
         size_t total_n_events = 0;
-        for (auto genEventCount : gen_counts){
-            const ntuple::GenId genId = genEventCount.first;
+        for (const auto& genEventCount : gen_counts){
             const size_t nevents = genEventCount.second;
             total_n_events += nevents;
-            std::cout << "GenId: " << genId.n_partons << " " << genId.n_b_partons << " " << genId.ht10_bin <<
-                         ", nevents: " << nevents << std::endl;
         }
-        std::cout << "Total events in the sumple: " << total_n_events << std::endl;
         return total_n_events;
     }
     size_t Integral(const DYBinDescriptor& binDescriptor_range) const
     {     
         size_t totalEvents = 0;
-        for (auto genEventCount : gen_counts){
-            const ntuple::GenId genId = genEventCount.first;
+        for (const auto& genEventCount : gen_counts){
+            const auto& genId = genEventCount.first;
             const size_t nevents = genEventCount.second;
-            if(binDescriptor_range.n_jet.Contains(genId.n_partons) &&
-                  binDescriptor_range.n_bjet.Contains(genId.n_b_partons) &&
-                  binDescriptor_range.n_ht.Contains(genId.ht10_bin))
+            if(binDescriptor_range.Contains(genId))
                 totalEvents += nevents;
             continue;
 
@@ -55,8 +51,8 @@ struct SampleDescriptor {
 
 class FileMerger { // simple analyzer definition
 public:
-    using GenMap = SampleDescriptor::GenMap;
-    using VSD = std::vector<SampleDescriptor>;
+    using GenMap = ntuple::GenEventCountMap;
+    using VSD = std::vector<SampleDescriptor<DYBinDescriptor, ntuple::GenEventCountMap>>;
     using VBD = std::vector<DYBinDescriptor>;
     FileMerger(const Arguments& _args) : args(_args)
     {
@@ -67,8 +63,8 @@ public:
 
 
 private:
-    SampleDescriptor global_map;
-    SampleDescriptor inclusive;
+    SampleDescriptor<DYBinDescriptor, ntuple::GenEventCountMap> global_map;
+    SampleDescriptor<DYBinDescriptor, ntuple::GenEventCountMap> inclusive;
     VSD all_samples;
     VBD output_bins;
 
@@ -90,7 +86,7 @@ private:
             std::cout << "File descriptor characteristics: " << file_descriptor.first << ", " <<
                          file_descriptor_element.file_path << ", " << file_descriptor_element.fileType
                       << std::endl;
-            SampleDescriptor sample_desc;
+            SampleDescriptor<DYBinDescriptor, ntuple::GenEventCountMap> sample_desc;
             sample_desc.bin = file_descriptor_element;
             //global_map.bin = file_descriptor_element;
             if (file_descriptor_element.fileType == analysis::FileType::inclusive)
@@ -123,81 +119,80 @@ private:
        output_bins = DYBinDescriptor::LoadConfig(args.cfg_name());
     }
 
-    void FindBestBin(DYBinDescriptor& output_bin) const
+    void CalculateWeight(DYBinDescriptor& output_bin) const
     {
+        size_t contribution = 0;
+        size_t other_contribution = 0;
+        size_t sample_contribution = 0;
+        bool use_complex_formula = false;
         for(unsigned i = 0; i < all_samples.size(); ++i) {
-            const SampleDescriptor sample = all_samples.at(i);
-            const size_t contribution = sample.Integral(output_bin);
+            const SampleDescriptor<DYBinDescriptor, ntuple::GenEventCountMap> sample = all_samples.at(i);
+            contribution = sample.Integral(output_bin);
             if(!contribution) continue;
             //formula 2
             PhysicalValue nu ( contribution / double(sample.Integral()), sqrt(double(contribution))/double(sample.Integral()));
-
             if(!(sample.bin.fileType == analysis::FileType::inclusive)) {
-                const size_t sample_contribution = inclusive.Integral(sample.bin);
+                other_contribution += contribution;
+                sample_contribution = inclusive.Integral(sample.bin);
                 // formula 3
                 PhysicalValue nu_incl(sample_contribution/double(inclusive.Integral()),
                                       sqrt(double(sample_contribution))/double(inclusive.Integral()));
                 nu *= nu_incl;
+                use_complex_formula = true;
             }
 
             if(output_bin.nu.GetStatisticalError() > nu.GetStatisticalError()) {
                 output_bin.nu = nu;
                 output_bin.ref_sample = sample.bin.name;
+                output_bin.fileType = sample.bin.fileType;
             }
+
         }
 
         if(output_bin.nu.GetStatisticalError() == std::numeric_limits<double>::infinity())
             throw exception("ref not found");
+
+        PhysicalValue n((double)contribution, std::sqrt((double)contribution));
+        PhysicalValue X((double)other_contribution,0);
+        PhysicalValue K((double)sample_contribution, std::sqrt((double)sample_contribution));
+        PhysicalValue sum = n + X;
+        size_t integral_ref_sample = 0;
+
+        for(unsigned i = 0; i < all_samples.size(); ++i) {
+            const SampleDescriptor<DYBinDescriptor, ntuple::GenEventCountMap> sample = all_samples.at(i);
+            if(sample.bin.fileType == output_bin.fileType)
+                integral_ref_sample = sample.Integral();
+        }
+
+        double error_weight = std::sqrt(std::pow((std::sqrt((double)contribution)*K.GetValue()*X.GetValue())
+                                                 /((double)integral_ref_sample*inclusive.Integral()*(X.GetValue()+n.GetValue())*(X.GetValue()+n.GetValue())),2)
+                                        + std::pow(((double)contribution*std::sqrt((double)sample_contribution))
+                                                   /((X.GetValue()+n.GetValue())*(double)integral_ref_sample*inclusive.Integral()),2));
+        if(!use_complex_formula){
+            PhysicalValue weight(output_bin.nu.GetValue() / sum.GetValue(),
+                                 (X.GetValue()/(X.GetValue()+n.GetValue())*(X.GetValue()+n.GetValue()))*
+                                 (std::sqrt((double)contribution)/double(inclusive.Integral())));
+            output_bin.weight = weight;
+        } else {
+            PhysicalValue weight(output_bin.nu.GetValue() / sum.GetValue(), (double)error_weight);
+            output_bin.weight = weight;
+        }
     }
 
-    void CalcWeight(DYBinDescriptor& output_bin) const
-    {
-        size_t inclusive_contribution = 0;
-        size_t other_contribution = 0;
-        for (unsigned i = 0; i < all_samples.size(); ++i) {
-            const SampleDescriptor sample = all_samples.at(i);
-            const size_t contribution = sample.Integral(output_bin);
-            if(!contribution) continue;
-            if(sample.bin.fileType == analysis::FileType::inclusive)
-                inclusive_contribution = contribution;
-            else
-                other_contribution += contribution;
-        }
-        PhysicalValue n((double)inclusive_contribution, std::sqrt((double)inclusive_contribution));
-        PhysicalValue X((double)other_contribution,0);
-        PhysicalValue sum = n + X;
-        output_bin.weight = output_bin.nu / sum;
-        //old definition
-        //output_bin.weight = output_bin.nu.GetValue() / global_map.Integral(output_bin);
-    }
+    //old definition
+    //output_bin.weight = output_bin.nu.GetValue() / global_map.Integral(output_bin);
 
     public:
 
 
     void Run()
     {
-
-        std::ofstream cfg(args.output_file());
-        if(cfg.fail())
-            throw analysis::exception("Unable to create outputfile'");
-        cfg.exceptions(std::ofstream::failbit | std::ofstream::badbit);
-
-        cfg << "#n_jet_min n_jet_max n_bjet_min n_bjet_max ht_bin_min ht_bin_max weight +/- err_w rel_err_w "
-               "nu +/- err_nu rel_err_nu ref_sample\n";
-
         for(unsigned i = 0; i < output_bins.size(); ++i)
         {
             DYBinDescriptor output_bin = output_bins.at(i);
-            FindBestBin(output_bin);
-            CalcWeight(output_bin);
-            //if(output_bin.nu.GetRelativeStatisticalError() >= 0.01)
-            cfg << output_bin.n_jet.min() << " " <<output_bin.n_jet.max()  << " " <<
-                   output_bin.n_bjet.min() << " " << output_bin.n_bjet.max() << " " <<
-                   output_bin.n_ht.min() << " " << output_bin.n_ht.max() << " " <<
-                   output_bin.weight << " " << output_bin.weight.GetRelativeStatisticalError() << " " <<
-                   output_bin.nu << " " << output_bin.nu.GetRelativeStatisticalError() <<
-                " " << output_bin.ref_sample <<  "\n";
+            CalculateWeight(output_bin);
         }
+        DYBinDescriptor::SaveCfg(args.output_file(), output_bins);
 
     }
 private:
