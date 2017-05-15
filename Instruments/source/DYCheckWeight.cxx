ab54b1067edd8661e07e1e152c103de1fb6c73f3
Maria Teresa <grippomariateresa@gmail.com>
2017-05-15 12:18:15 +0200

    Instruments/source/DYCheckWeight.cxx: implemented check weight for DY - not working

--- Instruments/source/DYCheckWeight.cxx
+++ Instruments/source/DYCheckWeight.cxx
@@ -8,8 +8,6 @@ This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 #include "Instruments/include/DYFileConfigEntryReader.h"
 #include "AnalysisTools/Core/include/NumericPrimitives.h"
 #include "Instruments/include/SampleDescriptor.h"
-#include "McCorrections/include/DY_weight.h"
-#include "McCorrections/include/EventWeights_HH.h"
 
 
 struct Arguments {
@@ -17,20 +15,30 @@ struct Arguments {
     run::Argument<std::string> input_path{"input_path", "Input path of the samples"};
     run::Argument<std::string> cfg_name{"cfg_name", "cfg bin splitting"};
     run::Argument<std::string> file_cfg_name{"file_cfg_name", "DY file cfg"};
-    run::Argument<std::string> output_file{"output_file", "Output file"};
 };
 
 namespace analysis {
 
 namespace sample_merging{
 
-class DYFileMerger {
+class DYCheckWeight {
 public:
     using GenMap = ntuple::GenEventCountMap;
     using VectorSampleDescriptor = std::vector<SampleDescriptor<DYBinDescriptor, ntuple::GenEventCountMap>>;
     using VectorDYBinDescriptor = std::vector<DYBinDescriptor>;
-    DYFileMerger(const Arguments& _args) : args(_args)
+    using Range_weight_map = std::map<Range<int>, double>;
+    using DoubleRange_map = std::map<Range<int>, Range_weight_map>;
+    using Event = ntuple::SummaryTuple;
+
+    DYCheckWeight(const Arguments& _args) : args(_args)
     {
+        std::vector<analysis::sample_merging::DYBinDescriptor> dy_descriptors =
+                analysis::sample_merging::DYBinDescriptor::LoadConfig(args.cfg_name());
+        for (unsigned n = 0; n < dy_descriptors.size(); ++n){
+            const analysis::sample_merging::DYBinDescriptor dybin_descriptor = dy_descriptors.at(n);
+            dy_weight_map[dybin_descriptor.n_jet][dybin_descriptor.n_bjet][dybin_descriptor.n_ht]
+                    = dybin_descriptor.weight.GetValue()/dybin_descriptor.inclusive_integral;
+        }
         LoadInputs();
     }
 
@@ -45,6 +53,7 @@ public:
 private:
 
     Arguments args;
+    std::map<Range<int>, DoubleRange_map> dy_weight_map;
 
 
     void LoadInputs()
@@ -77,7 +86,12 @@ private:
                     for (size_t i = 0; i < summaryTuple.data().lhe_n_partons.size(); ++i){ // loop on gen event info
 
                         size_t nevents = summaryTuple.data().lhe_n_events.at(i);
-                        totalWeight +=
+                        UInt_t n_partons = summaryTuple.data().lhe_n_partons.at(i);
+                        UInt_t n_b_partons =  summaryTuple.data().lhe_n_b_partons.at(i);
+                        UInt_t ht =  summaryTuple.data().lhe_ht10_bin.at(i);
+                        double weight = GetWeight(n_partons,n_b_partons,ht);
+                        double weight_prime = nevents * weight;
+                        totalWeight += weight_prime;
 
                     } // end loop on gen event info
                 } //end loop on entries
@@ -85,6 +99,28 @@ private:
             } // end loop on files
 
         } //end loop n file_descriptors
+        std::cout << "Total weight: " << totalWeight << std::endl;
+
+    }
+
+    double GetWeight(Int_t n_partons, Int_t n_b_partons, Int_t ht)
+    {
+        for (auto iter : dy_weight_map){
+            Range<int> n_jet = iter.first;
+            if (!(n_jet.Contains(n_partons))) continue;
+            DoubleRange_map doubleRange_map = iter.second;
+            for (auto iter_1 : doubleRange_map){
+                Range<int> n_bjet = iter_1.first;
+                if (!(n_bjet.Contains(n_b_partons))) continue;
+                Range_weight_map range_weight_map = iter_1.second;
+                for (auto iter_2 : range_weight_map){
+                    Range<int> n_ht = iter_2.first;
+                    if (n_ht.Contains(ht))
+                        return iter_2.second;
+                }
+            }
+        }
+        throw analysis::exception("drell-yan merge weight not found.");
     }
 
 
@@ -94,4 +130,4 @@ private:
 
 } //namespace analysis
 
-PROGRAM_MAIN(analysis::sample_merging::DYFileMerger, Arguments)
+PROGRAM_MAIN(analysis::sample_merging::DYCheckWeight, Arguments)
