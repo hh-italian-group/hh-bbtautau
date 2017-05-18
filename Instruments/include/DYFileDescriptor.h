36b9884fbfd4e54694438a54ae836301ee3a19d3
Maria Teresa <grippomariateresa@gmail.com>
2017-05-18 12:19:26 +0200

    Modified column widths for DYFileDescriptor and TTFileDescriptor (for TT to be defined better)

--- Instruments/include/DYFileDescriptor.h
+++ Instruments/include/DYFileDescriptor.h
@@ -84,41 +84,59 @@ struct DYBinDescriptor {
         std::vector<std::string> headers = { "#n_jet_min","n_jet_max","n_bjet_min","n_bjet_max",
                                            "ht_bin_min","ht_bin_max","weight","rel_err_w","nu",
                                            "rel_err_nu","incl_integral","ref_sample"};
-        const std::vector<int> column_widths(headers.size());
+        std::cout << "headers size:" << headers.size() << std::endl;
 
-        //for(n = 0..5) column_widths[n] = headers[n].size()
-        //for(n 5...) widths[n] = std::numeric_limits<double>::digits10 + 2
+        std::vector<int> column_widths_2(headers.size());
 
-        static const std::vector<int> column_widths = { 10,9,10,10,10,10,21,21,21,21,21,26 };
+        std::cout << "column_widths size:" << column_widths_2.size() << std::endl;
 
-        cfg << std::setw(column_widths.at(0)) << "#n_jet_min" <<
-               std::setw(column_widths.at(1)) << "n_jet_max" <<
-               std::setw(column_widths.at(2)) << "n_bjet_min" <<
-               std::setw(column_widths.at(3)) << "n_bjet_max" <<
-               std::setw(column_widths.at(4)) << "ht_bin_min" <<
-               std::setw(column_widths.at(5)) << "ht_bin_max" <<
-               std::setw(column_widths.at(6)) << "weight" <<
-               std::setw(column_widths.at(7)) << "rel_err_w" <<
-               std::setw(column_widths.at(8)) << "nu" <<
-               std::setw(column_widths.at(9)) << "rel_err_nu" <<
-               std::setw(column_widths.at(10)) << "incl_integral" <<
-               std::setw(column_widths.at(11)) << "ref_sample\n";
+        column_widths_2[0] = headers[0].size();
+        for(unsigned n = 1; n <= 5; ++n) {
+            column_widths_2[n] = headers[n].size()+1;
+            std::cout << "column_widths n:" << column_widths_2[n] << std::endl;
+        }
 
-        cfg << std::setprecision(std::numeric_limits<double>::digits10 + 1);
+//        for(unsigned h = 6; h <= 9; ++h){
+        for(unsigned h = 6; h < headers.size(); ++h){
+          column_widths_2[h] = std::numeric_limits<double>::digits10 + 6;
+          std::cout << "column_widths n:" << column_widths_2[h] << std::endl;
+        }
+
+//        for(unsigned h = 10; h < headers.size(); ++h){
+//          column_widths_2[h] = headers[h].size()+1;
+//          std::cout << "column_widths n:" << column_widths_2[h] << std::endl;
+//        }
+
+//        static const std::vector<int> column_widths = { 10,9,10,10,10,10,21,21,21,21,21,26 };
+
+        cfg << std::setw(column_widths_2.at(0)) << "#n_jet_min" <<
+               std::setw(column_widths_2.at(1)) << "n_jet_max" <<
+               std::setw(column_widths_2.at(2)) << "n_bjet_min" <<
+               std::setw(column_widths_2.at(3)) << "n_bjet_max" <<
+               std::setw(column_widths_2.at(4)) << "ht_bin_min" <<
+               std::setw(column_widths_2.at(5)) << "ht_bin_max" <<
+               std::setw(column_widths_2.at(6)) << "weight" <<
+               std::setw(column_widths_2.at(7)) << "rel_err_w" <<
+               std::setw(column_widths_2.at(8)) << "nu" <<
+               std::setw(column_widths_2.at(9)) << "rel_err_nu" <<
+               std::setw(column_widths_2.at(10)) << "incl_integral" <<
+               std::setw(column_widths_2.at(11)) << "ref_sample\n";
+
+        cfg << std::setprecision(std::numeric_limits<double>::digits10);
         for(auto& output_bin : output_bins)
         {
-            cfg << std::setw(column_widths.at(0)) << output_bin.n_jet.min() << " " <<
-                   std::setw(column_widths.at(1)) << output_bin.n_jet.max()  << " " <<
-                   std::setw(column_widths.at(2)) << output_bin.n_bjet.min() << " " <<
-                   std::setw(column_widths.at(3)) << output_bin.n_bjet.max() << " " <<
-                   std::setw(column_widths.at(4)) << output_bin.n_ht.min() << " " <<
-                   std::setw(column_widths.at(5)) << output_bin.n_ht.max() << " " <<
-                   std::setw(column_widths.at(6)) << output_bin.weight.GetValue() << " " <<
-                   std::setw(column_widths.at(7)) << output_bin.weight.GetRelativeStatisticalError() << " " <<
-                   std::setw(column_widths.at(8)) << output_bin.nu.GetValue() << " " <<
-                   std::setw(column_widths.at(9)) << output_bin.nu.GetRelativeStatisticalError() << " " <<
-                   std::setw(column_widths.at(10)) << output_bin.inclusive_integral << " " <<
-                   std::setw(column_widths.at(11)) << output_bin.ref_sample <<  "\n";
+            cfg << std::setw(column_widths_2.at(0)) << output_bin.n_jet.min() <<
+                   std::setw(column_widths_2.at(1)) << output_bin.n_jet.max()  <<
+                   std::setw(column_widths_2.at(2)) << output_bin.n_bjet.min() <<
+                   std::setw(column_widths_2.at(3)) << output_bin.n_bjet.max() <<
+                   std::setw(column_widths_2.at(4)) << output_bin.n_ht.min() <<
+                   std::setw(column_widths_2.at(5)) << output_bin.n_ht.max() <<
+                   std::setw(column_widths_2.at(6)) << output_bin.weight.GetValue() <<
+                   std::setw(column_widths_2.at(7)) << output_bin.weight.GetRelativeStatisticalError() <<
+                   std::setw(column_widths_2.at(8)) << output_bin.nu.GetValue() <<
+                   std::setw(column_widths_2.at(9)) << output_bin.nu.GetRelativeStatisticalError() <<
+                   std::setw(column_widths_2.at(10)) << output_bin.inclusive_integral <<
+                   std::setw(column_widths_2.at(11)) << output_bin.ref_sample <<  "\n";
         }
         return cfg;
     }
