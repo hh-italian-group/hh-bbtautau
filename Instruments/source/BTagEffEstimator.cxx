76f8d689f524d77c20ae22ef7fda1d2b118a144a
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2017-09-29 15:05:07 +0200

    Final version of the code

--- Instruments/source/BTagEffEstimator.cxx
+++ Instruments/source/BTagEffEstimator.cxx
@@ -15,53 +15,39 @@ This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 #include "AnalysisTools/Core/include/TextIO.h"
 
 struct Arguments { // list of all program arguments
-    REQ_ARG(std::string, output_file); 
-    OPT_ARG(bool, apply_tau_id_cut, true); 
-    OPT_ARG(std::string, apply_pu_id_cut,"");
+    REQ_ARG(std::string, output_file);  
+    OPT_ARG(std::string, apply_pu_id_cut,"no");
     REQ_ARG(std::vector<std::string>, input_file); 
 };
 
+namespace analysis {
+
 class BTagData : public root_ext::AnalyzerData {
 public:
-    /*explicit BTagData(std::shared_ptr<TFile> _outputFile, const std::string& directoryName = "") :
-        AnalyzerData::(_outputFile, directoryName)
-        {
-            static const std::vector<double> x_bins = {20,30,40,60,100,150,200,300,1000};
-            static const std::vector<double> y_bins = {0,0.6,1.2,2.1,2.4}; 
-            h2.SetMasterHist(x_bins, y_bins);
-            eff.SetMasterHist(x_bins, y_bins);
-            validate.SetMasterHist(x_bins, y_bins);
-            val_check.SetMasterHist(x_bins, y_bins);
-        }*/
-        using AnalyzerData::AnalyzerData;
-        const std::vector<double> x_bins = {20,30,40,60,100,150,200,300,1000};
-        const std::vector<double> y_bins = {0,0.6,1.2,2.1,2.4}; 
-        TH2D_ENTRY_CUSTOM(h2,x_bins,y_bins)
-        TH2D_ENTRY_CUSTOM(eff,x_bins,y_bins)
-        TH2D_ENTRY_CUSTOM(validate,x_bins,y_bins)
-        TH2D_ENTRY_CUSTOM(val_check,x_bins,y_bins)
+    using AnalyzerData::AnalyzerData;
+    const std::vector<double> x_bins = {20,30,40,60,100,150,200,300,1000};
+    const std::vector<double> y_bins = {0,0.6,1.2,2.1,2.4};
+    TH2D_ENTRY_CUSTOM(h2,x_bins,y_bins)
+    TH2D_ENTRY_CUSTOM(eff,x_bins,y_bins)
+    TH2D_ENTRY_CUSTOM(validate,x_bins,y_bins)
+    TH2D_ENTRY_CUSTOM(val_check,x_bins,y_bins)
 };
 
 class BTagEfficiency { 
 public:
     using Event = ntuple::Event;
     using EventTuple = ntuple::EventTuple;
-    using EventEnergyScale = analysis::EventEnergyScale;
-    using Channel = analysis::Channel; 
-    using DiscriminatorWP = analysis::DiscriminatorWP; 
-
-
 
     BTagEfficiency(const Arguments& _args) : args(_args), 
     outfile(root_ext::CreateRootFile(args.output_file()))
     {
-       // for (const auto& folder: folderMap)
-       //       anaDataMap[folder.first] = std::make_shared<BTagData>(outfile,folder.second);
-        anaDataMap["All"] = std::make_shared<BTagData>(outfile,"All");      
+        anaDataMap["All"] = std::make_shared<BTagData>(outfile,"All");
         for(const auto& tau_sign : tau_signs){
             for (const auto& tau_iso : tau_isos){
-                anaDataMap[tau_sign+tau_iso+eff] = std::make_shared<BTagData>(outfile,tau_sign+"/"+tau_iso+"/"+effMap[eff]);
-                anaDataMap[tau_sign+tau_iso+valCha] = std::make_shared<BTagData>(outfile,tau_sign+"/"+tau_iso+"/"+valMap[valCha]);
+                anaDataMap[tau_sign+tau_iso+eff] = std::make_shared<BTagData>(
+                            outfile,tools::FullPath({tau_sign, tau_iso, effMap[eff]}));
+                anaDataMap[tau_sign+tau_iso+valCha] = std::make_shared<BTagData>(
+                            outfile,tools::FullPath({tau_sign,tau_iso,valMap[valCha]}));
             }
             anaDataMap[tau_sign+valIso] = std::make_shared<BTagData>(outfile,tau_sign+"/"+valMap[valIso]);
         }
@@ -111,9 +97,7 @@ public:
                     auto bb = event.jets_p4.at(0) + event.jets_p4.at(1);
                     if (!cuts::hh_bbtautau_2016::hh_tag::IsInsideEllipse(event.SVfit_p4.mass(),bb.mass())) continue;
 
-                    std::string tau_sign = "";
-                    if ((event.q_1+event.q_2) == 0) tau_sign = "OS";
-                    else tau_sign = "SS";
+                    std::string tau_sign = (event.q_1+event.q_2) == 0 ? "OS" : "SS";
 
                     static const std::string tau_iso_disc = "byMediumIsolationMVArun2v1DBoldDMwLT";
                     static const uint32_t tau_iso_disc_hash = analysis::tools::hash(tau_iso_disc);
@@ -221,45 +205,47 @@ public:
 
                             //Make Validation plots between tau signs
                             for(auto tau_sign2 = std::next(tau_sign1); tau_sign2 != tau_signs.end(); ++tau_sign2){
-                                //if(tau_sign1 == tau_sign2) continue;
-                                anaDataMap[valQ+*tau_iso1]->validate(jet_flavour,btag_wp.first,*channel1,*tau_sign2,*tau_sign1).
-                                    CopyContent(anaDataMap[*tau_sign2+*tau_iso1+eff]->eff(jet_flavour, btag_wp.first,*channel1));
-                                anaDataMap[valQ+*tau_iso1]->validate(jet_flavour,btag_wp.first,*channel1,*tau_sign2,*tau_sign1).
-                                    Add(&anaDataMap[*tau_sign1+*tau_iso1+eff]->eff(jet_flavour, btag_wp.first,*channel1),-1.0);
-                                checkValidation(anaDataMap[valQ+*tau_iso1]->validate(jet_flavour,btag_wp.first,*channel1,
-                                            *tau_sign2,*tau_sign1),valMap[valQ]+"/"+*tau_iso1+"/val_"+jet_flavour+"_"+
-                                        btag_wp.first+"_"+*channel1+"_"+*tau_sign2+"_"+*tau_sign1,
+
+                                anaDataMap[valQ+*tau_iso1]->validate(jet_flavour,btag_wp.first,*channel1,*tau_sign2,
+                                        *tau_sign1).CopyContent(anaDataMap[*tau_sign2+*tau_iso1+eff]->
+                                        eff(jet_flavour, btag_wp.first,*channel1));
+                                anaDataMap[valQ+*tau_iso1]->validate(jet_flavour,btag_wp.first,*channel1,*tau_sign2,
+                                        *tau_sign1).Add(&anaDataMap[*tau_sign1+*tau_iso1+eff]->
+                                        eff(jet_flavour, btag_wp.first,*channel1),-1.0);
+                                checkValidation(anaDataMap[valQ+*tau_iso1]->validate(jet_flavour,btag_wp.first,
+                                        *channel1,*tau_sign2,*tau_sign1),tools::FullPath({valMap[valQ],*tau_iso1}),
                                         anaDataMap[valQ+*tau_iso1]->val_check(jet_flavour,btag_wp.first,*channel1,
                                             *tau_sign2,*tau_sign1));
                             }
-
                    
                             //Make Vlaidation plots between Isolations
                             for (auto tau_iso2 = std::next(tau_iso1); tau_iso2 != tau_isos.end(); ++tau_iso2){
-                                //if(tau_iso1 == tau_iso2) continue;
-                                anaDataMap[*tau_sign1+valIso]->validate(jet_flavour,btag_wp.first,*channel1,*tau_iso2,*tau_iso1).
-                                    CopyContent(anaDataMap[*tau_sign1+*tau_iso2+eff]->eff(jet_flavour, btag_wp.first,*channel1));
-                                anaDataMap[*tau_sign1+valIso]->validate(jet_flavour,btag_wp.first,*channel1,*tau_iso2,*tau_iso1).
-                                    Add(&anaDataMap[*tau_sign1+*tau_iso1+eff]->eff(jet_flavour, btag_wp.first,*channel1),-1.0);
+
+                                anaDataMap[*tau_sign1+valIso]->validate(jet_flavour,btag_wp.first,*channel1,*tau_iso2,
+                                        *tau_iso1).CopyContent(anaDataMap[*tau_sign1+*tau_iso2+eff]->
+                                        eff(jet_flavour, btag_wp.first,*channel1));
+                                anaDataMap[*tau_sign1+valIso]->validate(jet_flavour,btag_wp.first,*channel1,*tau_iso2,
+                                        *tau_iso1).Add(&anaDataMap[*tau_sign1+*tau_iso1+eff]->
+                                        eff(jet_flavour, btag_wp.first,*channel1),-1.0);
                                 checkValidation(anaDataMap[*tau_sign1+valIso]->validate(jet_flavour,btag_wp.first,
-                                            *channel1,*tau_iso2,*tau_iso1),*tau_sign1+"/"+valMap[valIso]+"/val_"+
-                                        jet_flavour+"_"+btag_wp.first+"_"+*channel1+"_"+*tau_iso2+"_"+*tau_iso1,
+                                            *channel1,*tau_iso2,*tau_iso1),tools::FullPath({*tau_sign1,valMap[valIso]}),
                                         anaDataMap[*tau_sign1+valIso]->val_check(jet_flavour,btag_wp.first,
                                             *channel1,*tau_iso2,*tau_iso1));
                             }
                 
                             //Make Validation plots between Channels 
                             for (auto channel2 = std::next(channel1); channel2 != channel_names.end(); ++channel2){
-                               // if (channel1 == channel2) continue;
-                                anaDataMap[*tau_sign1+*tau_iso1+valCha]->validate(jet_flavour,btag_wp.first,*channel2,*channel1).
-                                    CopyContent(anaDataMap[*tau_sign1+*tau_iso1+eff]->eff(jet_flavour, btag_wp.first,*channel2));
-                                anaDataMap[*tau_sign1+*tau_iso1+valCha]->validate(jet_flavour,btag_wp.first,*channel2,*channel1).
-                                    Add(&anaDataMap[*tau_sign1+*tau_iso1+eff]->eff(jet_flavour, btag_wp.first,*channel1),-1.0);
+
+                                anaDataMap[*tau_sign1+*tau_iso1+valCha]->validate(jet_flavour,btag_wp.first,*channel2,
+                                        *channel1).CopyContent(anaDataMap[*tau_sign1+*tau_iso1+eff]->
+                                        eff(jet_flavour, btag_wp.first,*channel2));
+                                anaDataMap[*tau_sign1+*tau_iso1+valCha]->validate(jet_flavour,btag_wp.first,*channel2,
+                                        *channel1).Add(&anaDataMap[*tau_sign1+*tau_iso1+eff]->
+                                        eff(jet_flavour, btag_wp.first,*channel1),-1.0);
                                 checkValidation(anaDataMap[*tau_sign1+*tau_iso1+valCha]->validate(jet_flavour,
-                                            btag_wp.first,*channel2,*channel1),*tau_sign1+"/"+*tau_iso1+"/"+valMap[valCha]+
-                                        "/val_"+jet_flavour+"_"+btag_wp.first+"_"+*channel2+"_"+*channel1,
-                                        anaDataMap[*tau_sign1+*tau_iso1+valCha]->val_check(jet_flavour,
-                                            btag_wp.first,*channel2,*channel1));
+                                        btag_wp.first,*channel2,*channel1),tools::FullPath({*tau_sign1,*tau_iso1,
+                                        valMap[valCha]}),anaDataMap[*tau_sign1+*tau_iso1+valCha]->val_check(jet_flavour,
+                                        btag_wp.first,*channel2,*channel1));
                             }
                         }
                     }
@@ -305,9 +291,10 @@ private:
         return true; 
     }
     
-    static void checkValidation(const TH2D& validation_histo, const std::string histo_name,TH2D& output_histo){
+    static void checkValidation(const TH2D& validation_histo, const std::string histo_path,TH2D& output_histo){
         int nbinsX = validation_histo.GetNbinsX(); 
-        int nbinsY = validation_histo.GetNbinsY(); 
+        int nbinsY = validation_histo.GetNbinsY();
+        std::string histo_name = tools::FullPath({histo_path,validation_histo.GetName()});
         for(int i=1;i<= nbinsX;i++){
             for(int j=1;j<=nbinsY;j++){
                 double xPosition = validation_histo.GetXaxis()->GetBinCenter(i);
@@ -318,11 +305,13 @@ private:
                 if (n_sigma < 0) n_sigma = n_sigma+1;
                 output_histo.SetBinContent(i,j,n_sigma);
                 if(n_sigma !=0) std::cout<<n_sigma<<" sigma: "<<histo_name<<" in (pt,eta) bin ("<<
-                    xPosition<<","<<yPosition<<"):"<<value<<"+-"<<error<<std::endl;
+                    xPosition<<","<<yPosition<<"):"<<value<<" +- "<<error<<std::endl;
             }
         }
     }
     
 };
 
-PROGRAM_MAIN(BTagEfficiency, Arguments) // definition of the main program function
+} // namespace analysis
+
+PROGRAM_MAIN(analysis::BTagEfficiency, Arguments) // definition of the main program function
