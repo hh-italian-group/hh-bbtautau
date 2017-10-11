f7a39736fc4f023102a4697207044b6b92e9deac
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2017-10-11 16:02:21 +0200

    Preliminary mass cut

--- Analysis/source/bbmumuAnalyzer.cxx
+++ Analysis/source/bbmumuAnalyzer.cxx
@@ -14,18 +14,22 @@ protected:
     virtual EventRegion DetermineEventRegion(EventInfo& event, EventCategory /*eventCategory*/) override
     {
         static const std::vector<std::string> trigger_patterns = {
-            "HLT_IsoMu22_eta2p1_v", "HLT_IsoTkMu22_eta2p1_v", "HLT_IsoMu22_v", "HLT_IsoTkMu22_v"
+            "HLT_IsoMu22_v"
         };
 
         const MuonCandidate& muon1 = event.GetFirstLeg();
         const MuonCandidate& muon2 = event.GetSecondLeg();
+        const JetCandidate& jet1  = event.GetJets().at(0);
+        const JetCandidate& jet2 = event.GetJets().at(1);
 
-        bool passTrigger = event.GetTriggerResults().AnyAcceptAndMatch(trigger_patterns);
-        std::cout<<"Trigger Results = "<<passTrigger<<std::endl;
         if(!event.GetTriggerResults().AnyAcceptAndMatch(trigger_patterns)) return EventRegion::Unknown();
 
         const bool os = !ana_setup.apply_os_cut || muon1.GetCharge() * muon2.GetCharge() == -1;
         const bool iso = true;
+        double mass_muMu = (muon1.GetMomentum()+muon2.GetMomentum()).M();
+        double mass_jj = (jet1.GetMomentum()+jet2.GetMomentum()).M();
+        const bool jetMass = (mass_jj > 80 && mass_jj < 160);
+        const bool muonMass= (mass_muMu > 60);
         return EventRegion(os, iso);
     }
 
