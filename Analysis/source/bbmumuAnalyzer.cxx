5a58703703437daf5c9091c425a5eec4ae46ee85
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2017-10-12 11:42:32 +0200

    Correct collection for jet and added the pT_miss cut

--- Analysis/source/bbmumuAnalyzer.cxx
+++ Analysis/source/bbmumuAnalyzer.cxx
@@ -9,7 +9,7 @@ class bbmumuAnalyzer : public BaseEventAnalyzer<MuonCandidate, MuonCandidate> {
 public:
     using Base = BaseEventAnalyzer<MuonCandidate, MuonCandidate>;
     using Base::BaseEventAnalyzer;
-
+    using HiggsBBCandidate = EventInfoBase::HiggsBBCandidate;
 protected:
     virtual EventRegion DetermineEventRegion(EventInfo& event, EventCategory /*eventCategory*/) override
     {
@@ -19,17 +19,18 @@ protected:
 
         const MuonCandidate& muon1 = event.GetFirstLeg();
         const MuonCandidate& muon2 = event.GetSecondLeg();
-        const JetCandidate& jet1  = event.GetJets().at(0);
-        const JetCandidate& jet2 = event.GetJets().at(1);
+        const HiggsBBCandidate& jets  = event.GetHiggsBB();
 
         if(!event.GetTriggerResults().AnyAcceptAndMatch(trigger_patterns)) return EventRegion::Unknown();
 
         const bool os = !ana_setup.apply_os_cut || muon1.GetCharge() * muon2.GetCharge() == -1;
         const bool iso = true;
         double mass_muMu = (muon1.GetMomentum()+muon2.GetMomentum()).M();
-        double mass_jj = (jet1.GetMomentum()+jet2.GetMomentum()).M();
+        double mass_jj = jets.GetMomentum().M();
         const bool jetMass = (mass_jj > 80 && mass_jj < 160);
         const bool muonMass= (mass_muMu > 60);
+        if(!jetMass || !muonMass) return EventRegion::Unknown();
+        if(event.GetMET().GetMomentum().Pt() > 45 ) return EventRegion::Unknown();
         return EventRegion(os, iso);
     }
 
