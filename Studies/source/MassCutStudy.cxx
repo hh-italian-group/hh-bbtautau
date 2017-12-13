ad56afaae651bfc8a53b98d74cd53a6f617dc29c
Angela Giraldi <giraldiangela@gmail.com>
2017-12-13 13:58:33 +0100

    New functions to find centre and radius

--- Studies/source/MassCutStudy.cxx
+++ Studies/source/MassCutStudy.cxx
@@ -3,6 +3,7 @@ This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
 
 #include <random>
 #include "Math/Minimizer.h"
+#include "Minuit2/Minuit2Minimizer.h"
 #include "Math/Factory.h"
 #include "Math/Functor.h"
 #include "AnalysisTools/Run/include/program_main.h"
@@ -62,35 +63,112 @@ public:
     double N_bkg{0}, N_sgn{0};
     double max_histo_mbb, max_histo_mtt;
 
+    static bool IsInsideEllipse(double x, double y, double x0, double y0, double a, double b)
+    {
+        return pow(x - x0, 2) / pow(a, 2) + pow(y - y0, 2) / pow(b, 2) < 1.;
+    }
+
     void TimeReport(bool tot = false) const
     {
         reporter->TimeReport(tot);
     }
 
-    double BkgMin (const double* param) const {
+    double BkgMin (double x0, double y0, const double* param) const {
         static const double k = 1000;
         const double a = param[0];
         const double b = param[1];
         double n_b = 0, n_s = 0;
 
+        if(a < 0 || b < 0 || x0 < 0 || y0 < 0) return 2;
+
         for (const auto& entry: element.at(SampleId::MassTot())){
-            auto temp = pow(entry.mbb-param[2],2)/pow(a,2) + pow(entry.mtt-param[3],2)/pow(b,2);
-            if (temp<1) n_s += entry.weight;
+            if(IsInsideEllipse(entry.mbb, entry.mtt, x0, y0, a, b))
+                n_s += entry.weight;
         }
         for (const auto& entry: element.at(SampleId::Bkg())){
-            auto temp = pow(entry.mbb-param[2],2)/pow(a,2) + pow(entry.mtt-param[3],2)/pow(b,2) ;
-            if (temp<1) n_b += entry.weight;
+            if(IsInsideEllipse(entry.mbb, entry.mtt, x0, y0, a, b))
+                n_b += entry.weight;
         }
         double result = n_b / N_bkg;
         if( args.efficiency() > n_s / N_sgn)
-            result += std::exp(k*std::pow(args.efficiency()-n_s/N_sgn, 2)) - 1;
+            result += 2*std::erf(k*(args.efficiency()-n_s/N_sgn));
+//            result += std::exp(k*std::pow(args.efficiency()-n_s/N_sgn, 2)) - 1;
 
-        std::cout<<"a: "<<a<<" b: "<<b<<"  eff_s: "<<n_s/N_sgn<<"   eff_b"<<n_b / N_bkg<<"  result:"<<result<<std::endl;
+        std::cout <<"a: "<< a << " b: "<< b << " x0: " << x0 << " y0: " << y0
+                  <<"  eff_s: "<<n_s/N_sgn<<"   eff_b"<<n_b / N_bkg<<"  result:"<< result << "\n";
         return result;
     }
 
+    double SignalMax(double a, double b, const double* param) const
+    {
+        const double x0 = param[0];
+        const double y0 = param[1];
+        double n_s = 0;
+
+        if(a < 0 || b < 0 || x0 < 0 || y0 < 0) return std::numeric_limits<double>::infinity();
+        for (const auto& entry: element.at(SampleId::MassTot())){
+            if(IsInsideEllipse(entry.mbb, entry.mtt, x0, y0, a, b))
+                n_s += entry.weight;
+        }
+        std::cout <<"a: "<< a << " b: "<< b << " x0: " << x0 << " y0: " << y0
+                  <<"  eff_s: "<<n_s/N_sgn << "\n";
+
+        return -n_s / N_sgn;
+    }
+
+
+    void FindCenter(double a, double b, double& x0, double& y0, double step) const
+    {
+        auto min = std::shared_ptr<ROOT::Math::Minimizer>(ROOT::Math::Factory::CreateMinimizer("Minuit", "Combined"));
+        min->SetMaxFunctionCalls(100000);
+        min->SetMaxIterations(100000);
+        min->SetPrintLevel(2);
+//        min->SetStrategy(2);
+        min->SetTolerance(0.1);
+        min->SetPrecision(0.01);
+
+        auto fn = std::bind(&MassCutStudy::SignalMax, this, a, b, std::placeholders::_1);
+        ROOT::Math::Functor f(fn, 2);
+        min->SetFunction(f);
+        min->SetVariable(0,"max_mbb", x0, step);
+        min->SetVariable(1,"max_mtt", y0, step);
+
+
+        min->Minimize();
+
+        x0 = min->X()[0];
+        y0 = min->X()[1];
+    }
+
+    void FindRadius(double x0, double y0, double& a, double& b, double step) const
+    {
+        auto min = std::shared_ptr<ROOT::Math::Minimizer>(ROOT::Math::Factory::CreateMinimizer("Minuit", "Combined"));
+        min->SetMaxFunctionCalls(100000);
+        min->SetMaxIterations(100000);
+        min->SetPrintLevel(2);
+        min->SetTolerance(0.05);
+        min->SetPrecision(0.01);
+
+        auto fn = std::bind(&MassCutStudy::BkgMin, this, x0, y0, std::placeholders::_1);
+        ROOT::Math::Functor f(fn, 2);
+        min->SetFunction(f);
+        min->SetVariable (0,"a(bb)", a, step);
+        min->SetVariableLimits(0, 1, 100);
+        min->SetVariable(1,"b(tt)", b, step);
+        min->SetVariableLimits(1, 1, 150);
+
+
+        min->Minimize();
+
+        a = min->X()[0];
+        b = min->X()[1];
+    }
+
+
     void Run()
     {
+        std::cout << ROOT::Math::MinimizerOptions::DefaultMinimizerType() << " "
+                  << ROOT::Math::MinimizerOptions::DefaultMinimizerAlgo() << std::endl;
         for(const auto& entry : samples)
         {
             if ( entry.id.IsSignal() && entry.spin != args.spin()) continue;
@@ -105,11 +183,12 @@ public:
 //                if (!cuts::hh_bbtautau_2016::hh_tag::IsInsideMassWindow(eventbase.GetHiggsTTMomentum(true).mass(), eventbase.GetHiggsBB().GetMomentum().mass()))
 //                    continue;
                 const auto& Hbb = eventbase.GetHiggsBB().GetMomentum();
-                const auto& Htt_sv = eventbase.GetHiggsTTMomentum(true);
+//                const auto& Htt_sv = eventbase.GetHiggsTTMomentum(true);
                 const auto& Htt = eventbase.GetHiggsTTMomentum(false);
-                const auto& met = eventbase.GetMET().GetMomentum();
-                element[sampleid].emplace_back(Hbb.M(), (Htt_sv).M(), eventbase->weight_total);
+//                const auto& met = eventbase.GetMET().GetMomentum();
+//                element[sampleid].emplace_back(Hbb.M(), (Htt_sv).M(), eventbase->weight_total);
 //                element[sampleid].emplace_back(Hbb.M(), (Htt+met).M(), eventbase->weight_total);
+                element[sampleid].emplace_back(Hbb.M(), Htt.M(), eventbase->weight_total);
                 if (entry.id.IsBackground()) N_bkg += eventbase->weight_total;
                 if (entry.id.IsSignal()) N_sgn += eventbase->weight_total;
             }
@@ -136,26 +215,22 @@ public:
         std::cout<<element[SampleId::MassTot()].size()<<"   "<<element[SampleId::Bkg()].size()<<std::endl;
         std::cout<<N_sgn<<"   "<<N_bkg<<std::endl;
 
-        ROOT::Math::Minimizer* min =ROOT::Math::Factory::CreateMinimizer("", "");
-        min->SetMaxFunctionCalls(1000000);
-        min->SetMaxIterations(100000);
-        min->SetTolerance(0.01);
-        min->SetPrintLevel(2);
+        double a = histo_mbb->GetStdDev(), b = histo_mtt->GetStdDev();
+        double x0 = histo_mbb->GetBinCenter(histo_mbb->GetMaximumBin());
+        double y0 = histo_mtt->GetBinCenter(histo_mtt->GetMaximumBin());
+        FindCenter(a, b, x0, y0, 0.1);
+        std::cout << "x0: " << x0 << " y0: " << y0 << std::endl;
+        /*a *= 2; */b *= 2;
+        FindRadius(x0, y0, a, b, 0.01);
+        std::cout << "a: " << a << " b: " << b << std::endl;
 
-        auto fn = std::bind(&MassCutStudy::BkgMin, this, std::placeholders::_1);
-        ROOT::Math::Functor f(fn,4);
-        double step[4] = {0.1,0.1, 0.01,0.01};
-        double variable[4] = {150, 150, max_histo_mtt, max_histo_mbb};
-        min->SetFunction(f);
-        min->SetVariable(0,"a(bb)",variable[0], step[0]);
-        min->SetVariable(1,"b(tt)",variable[1], step[1]);
-        min->SetVariable(2,"max_mbb",variable[2], step[2]);
-        min->SetVariable(3,"max_mtt",variable[3], step[3]);
+        std::cout << "Minimum:\n";
+        double minimum[] = { a, b };
+        BkgMin(x0, y0, minimum);
 
-        min->Minimize();
-
-        const double *xs = min->X();
-        std::cout << "Minimum: f(" << xs[0] << "," << xs[1] << ","<< xs[2] << "," << xs[3] << "): "<< min->MinValue()  << std::endl;
+        std::cout << "AN:\n";
+        const double AN[] = { 45., 35. };
+        BkgMin(111., 116., AN);
 
         TimeReport(true);
     }
