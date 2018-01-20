2f52b6a17dd7d386083bbadde9e46a5d2256b725
Angelagiraldi <giraldiangela@gmail.com>
2018-01-20 16:51:37 +0100

    New trainings for non-resonant BSM scenarios (#58)
    * Added function to reweight SM to BSM
    * Added BSM trainings with different train-test division

--- Instruments/source/SMFileMerger.cxx
+++ Instruments/source/SMFileMerger.cxx
@@ -26,8 +26,17 @@ class SMFileMergerData : public root_ext::AnalyzerData {
 public:
     using AnalyzerData::AnalyzerData;
 
-    const std::vector<double> mhh_Bins{ 245, 270, 300, 330, 360, 390, 420, 450, 500, 550, 600, 700, 800, 1000, 10000 };
-    const std::vector<double> cosTheta_Bins{ 0.0, 0.4, 0.6, 0.8, 0.9, 1.0 };
+
+//    const std::vector<double> mhh_Bins{ 245, 270, 300, 330, 360, 390, 420, 450, 500, 550, 600, 700, 800, 1000, 10000 };
+//    const std::vector<double> cosTheta_Bins{ 0.0, 0.4, 0.6, 0.8, 0.9, 1.0 };
+
+    const std::vector<double> mhh_Bins{ 250,260,270,280,290,300,310,320,330,340,
+                                        350,360,370,380,390,400,410,420,430,440,
+                                        450,460,470,480,490,
+                                        500,510,520,530,540,550,600,610,620,630,
+                                        640,650,660,670,680,690,700,750,800,850,
+                                        900,950,1000,1100,1200,1300,1400,1500.,1750,2000,50000};
+    const std::vector<double> cosTheta_Bins{0.0, 0.4, 0.6, 0.8, 1.0} ;
 
     TH1D_ENTRY_CUSTOM(lhe_hh_m, mhh_Bins)
     TH1D_ENTRY_CUSTOM(lhe_hh_cosTheta, cosTheta_Bins)
@@ -83,41 +92,41 @@ private:
                     std::shared_ptr<ntuple::ExpressEvent> event(new ntuple::ExpressEvent(AllEventTuple->data()));
 
                     //std::cout << "Mhh: " << event->lhe_hh_m << ", cosTheta: " << event->lhe_hh_cosTheta << std::endl;
-                    anaData.lhe_hh_m(name).Fill(event->lhe_hh_m);
-                    anaData.lhe_hh_cosTheta(name).Fill(std::abs(event->lhe_hh_cosTheta));
-                    anaData.lhe_hh_cosTheta_vs_m(name).Fill(event->lhe_hh_m,std::abs(event->lhe_hh_cosTheta));
+//                    anaData.lhe_hh_m(name).Fill(event->lhe_hh_m);
+//                    anaData.lhe_hh_cosTheta(name).Fill(std::abs(event->lhe_hh_cosTheta));
+                    anaData.lhe_hh_cosTheta_vs_m().Fill(event->lhe_hh_m,std::abs(event->lhe_hh_cosTheta));
                 } //end loop on entries
 
             } //end loop on files
 //            const double scale = 1 / anaData.lhe_hh_cosTheta_vs_m(name).Integral(0,anaData.lhe_hh_cosTheta_vs_m(name).GetNbinsX()+1,
 //                                                                                 0, anaData.lhe_hh_cosTheta_vs_m(name).GetNbinsY()+1);
 //            anaData.lhe_hh_cosTheta_vs_m(name).Scale(scale);
-            RenormalizeHistogram(anaData.lhe_hh_cosTheta_vs_m(name), 1, true);
-            if (file_descriptor_element.fileType == FileType::sm)
-                name_sm = name;
+            RenormalizeHistogram(anaData.lhe_hh_cosTheta_vs_m(), 1, true);
+//            if (file_descriptor_element.fileType == FileType::sm)
+//                name_sm = name;
         } //end loop n file_descriptors
 
-        for (const auto& file_descriptor : file_descriptors) {
-            const std::string& name = file_descriptor.second.name;
-            const auto& hist = anaData.lhe_hh_cosTheta_vs_m(name);
-            const Int_t N = hist.GetNbinsX() + 1;
-            const Int_t H = hist.GetNbinsY() + 1;
-            for (Int_t n = 0; n <= N; ++n){
-                for (Int_t h = 0; h <= H; ++h){
-                    const bool zero_content = anaData.lhe_hh_cosTheta_vs_m(name).GetBinContent(n,h) == 0;
-                    const bool overflow_bin = n == 0 || n == N || h == 0 || h == H;
-                    if((zero_content && !overflow_bin) || (!zero_content && overflow_bin)) {
-                        std::ostringstream ss;
-                        const std::string prefix = overflow_bin ? "Non empty" : "Empty";
-                        ss << prefix << " bin in " << name << ": (" << n << ", " << h << ") with center at ("
-                           << hist.GetXaxis()->GetBinCenter(n) << ", " << hist.GetYaxis()->GetBinCenter(h) << ").";
-                        throw exception(ss.str());
-                    }
-                }
-            }
-            anaData.weight(name).CopyContent(anaData.lhe_hh_cosTheta_vs_m(name_sm));
-            anaData.weight(name).Divide(&anaData.lhe_hh_cosTheta_vs_m(name));
-        }
+//        for (const auto& file_descriptor : file_descriptors) {
+//            const std::string& name = file_descriptor.second.name;
+//            const auto& hist = anaData.lhe_hh_cosTheta_vs_m(name);
+//            const Int_t N = hist.GetNbinsX() + 1;
+//            const Int_t H = hist.GetNbinsY() + 1;
+//            for (Int_t n = 0; n <= N; ++n){
+//                for (Int_t h = 0; h <= H; ++h){
+//                    const bool zero_content = anaData.lhe_hh_cosTheta_vs_m(name).GetBinContent(n,h) == 0;
+//                    const bool overflow_bin = n == 0 || n == N || h == 0 || h == H;
+//                    if((zero_content && !overflow_bin) || (!zero_content && overflow_bin)) {
+//                        std::ostringstream ss;
+//                        const std::string prefix = overflow_bin ? "Non empty" : "Empty";
+//                        ss << prefix << " bin in " << name << ": (" << n << ", " << h << ") with center at ("
+//                           << hist.GetXaxis()->GetBinCenter(n) << ", " << hist.GetYaxis()->GetBinCenter(h) << ").";
+//                        throw exception(ss.str());
+//                    }
+//                }
+//            }
+//            anaData.weight(name).CopyContent(anaData.lhe_hh_cosTheta_vs_m(name_sm));
+//            anaData.weight(name).Divide(&anaData.lhe_hh_cosTheta_vs_m(name));
+//        }
     }
 };
 
