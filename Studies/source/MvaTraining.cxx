688eded0136fbde0dc02e7d810e03c3336564e18
Konstantin Androsov <konstantin.androsov@gmail.com>
2018-01-22 12:24:51 +0100

    Fixed compilation issues.

--- Studies/source/MvaTraining.cxx
+++ Studies/source/MvaTraining.cxx
@@ -436,7 +436,7 @@ public:
         std::cout<< "quante coppie massa-spin? "<<mass_spin.size() <<std::endl;
 
         std::uniform_int_distribution<size_t> it(0, mass_spin.size() - 1);
-        std::uniform_int_distribution<size_t> bp(0, 9);
+        std::uniform_int_distribution<int> bp(0, 9);
         std::cout<<bkg<<std::endl;
 
         for (const auto& s : set){
