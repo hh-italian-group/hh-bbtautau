75f3b1f62f33e9d0e33abf518965ac337fc595bf
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2018-05-18 12:28:27 +0200

    Update

--- Analysis/include/DYModel.h
+++ Analysis/include/DYModel.h
@@ -203,7 +203,7 @@ public:
             if(jet_found) norm_sf = scale_factor_maps.at(sample_wp.full_name);
             else norm_sf = scale_factor_maps.at(sample_wp.full_name+"_"+ToString(it->first.second)+NJet_suffix());
         }
-        dataIds[finalId] = std::make_tuple(weight * fractional_weight * pt_weight * norm_sf, event.GetMvaScore());
+        dataIds[finalId] = std::make_tuple(weight * norm_sf, event.GetMvaScore());
     }
 
     template<typename T>
