2320143871e19cdaca9c434e39d6e721c92ed1c1
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2018-10-14 21:43:26 +0530

    Update of the muMu analyzer to include QCD

--- Analysis/source/bbmumuAnalyzer.cxx
+++ Analysis/source/bbmumuAnalyzer.cxx
@@ -15,28 +15,44 @@ protected:
     virtual EventRegion DetermineEventRegion(EventInfo& event, EventCategory /*eventCategory*/) override
     {
         static const std::map<DiscriminatorWP,double> working_points = {
-            {DiscriminatorWP::Loose,0.2}, {DiscriminatorWP::Medium,0.15}
+            {DiscriminatorWP::Loose,2.0}, {DiscriminatorWP::Medium,0.15}
         };
 
         const MuonCandidate& muon1 = event.GetFirstLeg();
         const MuonCandidate& muon2 = event.GetSecondLeg();
 //        const HiggsBBCandidate& jets  = event.GetHiggsBB();
 
-        EventRegion region;
+        EventRegion region_muon1, region_muon2;
         const bool os = !ana_setup.apply_os_cut || muon1.GetCharge() * muon2.GetCharge() == -1;
-        region.SetCharge(os);
+        region_muon1.SetCharge(os);
+        region_muon2.SetCharge(os);
 
         for(auto wp = working_points.rbegin(); wp != working_points.rend(); ++wp) {
             if(muon1.GetIsolation() < wp->second) {
-                region.SetUpperIso(wp->first);
+                region_muon1.SetUpperIso(wp->first);
                 if(wp != working_points.rbegin())
-                    region.SetLowerIso((--wp)->first);
-                //break;
+                    region_muon1.SetLowerIso((--wp)->first);
+                break;
             }
         }
+
+        for(auto wp = working_points.rbegin(); wp != working_points.rend(); ++wp) {
+            if(muon2.GetIsolation() < wp->second) {
+                region_muon2.SetUpperIso(wp->first);
+                if(wp != working_points.rbegin())
+                    region_muon2.SetLowerIso((--wp)->first);
+                break;
+            }
+        }
+
         //region.SetLowerIso(DiscriminatorWP::Medium);
 
-        return region;
+        if(!region_muon1.HasLowerIso() || !region_muon2.HasLowerIso()) return EventRegion::Unknown();
+        if(region_muon1.GetLowerIso() >= DiscriminatorWP::Medium) return region_muon2;
+        if(region_muon2.GetLowerIso() >= DiscriminatorWP::Medium) return region_muon1;
+        return EventRegion::Unknown();
+
+        //return region;
     }
 
     virtual EventSubCategory DetermineEventSubCategory(EventInfo& event, const EventCategory& /*category*/,
