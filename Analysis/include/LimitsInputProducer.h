351a4e3993e0fb4d55cc9419a6b1be785b2505d2
Maria Rosaria Di Domenico <maria.rosaria.di.domenico@cern.ch>
2018-11-12 16:39:48 +0100

    Fixing names

--- Analysis/include/LimitsInputProducer.h
+++ Analysis/include/LimitsInputProducer.h
@@ -64,7 +64,7 @@ public:
     }
 
 
-    void Produce(const std::string& outputFileNamePrefix, const std::string& category,
+    void Produce(const std::string& outputFileNamePrefix, const std::string& setup_name,
                  const std::map<EventCategory, std::string>& eventCategories, EventSubCategory eventSubCategory,
                  const EventEnergyScaleSet& eventEnergyScales, const EventRegionSet& eventRegions,
                  const std::map<SelectionCut, std::string>& sel_aliases)
@@ -74,7 +74,7 @@ public:
         static const std::string dirNamePrefix = ToString(anaDataCollection->ChannelId()) + "_";
 
         std::ostringstream s_file_name;
-        s_file_name << outputFileNamePrefix << "_" << category;
+        s_file_name << outputFileNamePrefix << "_" << setup_name;
         if(eventSubCategory != EventSubCategory::NoCuts())
             s_file_name << "_" << eventSubCategory.ToString(sel_aliases);
         const std::string file_name = s_file_name.str();
@@ -84,7 +84,8 @@ public:
         for(const EventAnalyzerDataId& metaId : EventAnalyzerDataId::MetaLoop(eventCategories, eventEnergyScales,
                                                                               sampleWorkingPoints, eventRegions))
         {
-            const std::string directoryName = dirNamePrefix + category + EventRegionSuffix(metaId.Get<EventRegion>());
+            const std::string directoryName = dirNamePrefix + ToString(metaId.Get<EventCategory>()) +
+                                              EventRegionSuffix(metaId.Get<EventRegion>());
             TDirectory* directory = root_ext::GetDirectory(*outputFile, directoryName, true);
             const SampleWP& sampleWP = sampleWorkingPoints.at(metaId.Get<std::string>());
             const auto anaDataId = metaId.Set(eventSubCategory);
