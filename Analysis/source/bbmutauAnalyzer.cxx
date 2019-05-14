dfb60c2ddc2327e7576e4c4b34f490e7ee998f60
Maria Teresa Grippo <mgrippo@users.noreply.github.com>
2019-05-14 15:46:16 +0200

    Added the possibility to have tauID discriminator and WP from config. (#109)
    Added QCD estimation in mutau analyzer as in HTT analysis.
    Added for Skimmer lighter selection

--- Analysis/source/bbmutauAnalyzer.cxx
+++ Analysis/source/bbmutauAnalyzer.cxx
@@ -12,8 +12,12 @@ public:
 protected:
     virtual EventRegion DetermineEventRegion(EventInfoBase& eventInfoBase, EventCategory /*eventCategory*/) override
     {
-        static const std::vector<DiscriminatorWP> working_points = {
-            DiscriminatorWP::VLoose, DiscriminatorWP::Loose, DiscriminatorWP::Medium
+        //static const std::vector<DiscriminatorWP> working_points = {
+        //    DiscriminatorWP::VLoose, DiscriminatorWP::Loose, DiscriminatorWP::Medium
+        //};
+
+	static const std::map<DiscriminatorWP,double> working_points = {
+            {DiscriminatorWP::VVLoose,2.0}, {DiscriminatorWP::Medium,0.15}
         };
 
         const LepCandidate& muon = eventInfoBase.GetFirstLeg();
@@ -24,20 +28,39 @@ protected:
         if(ana_setup.mode == SignalMode::HTT || ana_setup.mode == SignalMode::HTT_sync){
             double mt = analysis::Calculate_MT(muon.GetMomentum(),eventInfoBase.GetMET().GetMomentum());
             if(mt >= cuts::H_tautau_2016::mt) return EventRegion::Unknown();
+	    double m_tt_vis = (muon.GetMomentum() + tau.GetMomentum()).M();
+	    if(m_tt_vis <= 50) return EventRegion::Unknown();
+	    //cut for tau ID SF compatibility
+	    //double pzeta = analysis::Calculate_Pzeta(muon.GetMomentum(),tau.GetMomentum(),eventInfoBase.GetMET().GetMomentum());
+	    //if(pzeta <= -25) return EventRegion::Unknown();
         }
 
         const bool os = !ana_setup.apply_os_cut || muon.GetCharge() * tau.GetCharge() == -1;
         region.SetCharge(os);
 
-        for(auto wp = working_points.rbegin(); wp != working_points.rend(); ++wp) {
-            if(tau->Passed(TauIdDiscriminator::byIsolationMVArun2017v2DBoldDMwLT2017, *wp)) {
-                region.SetLowerIso(*wp);
+	if(!tau->Passed(ana_setup.tauIdDiscriminator, ana_setup.workingPoint)) return EventRegion::Unknown();
+
+        //for(auto wp = working_points.rbegin(); wp != working_points.rend(); ++wp) {
+          //  if(tau->Passed(TauIdDiscriminator::byIsolationMVArun2017v2DBoldDMwLT2017, *wp)) {
+            //    region.SetLowerIso(*wp);
+            //    if(wp != working_points.rbegin())
+            //        region.SetUpperIso(*(--wp));
+            //    break;
+            //}
+        //}
+
+        //return region;
+
+	for(auto wp = working_points.rbegin(); wp != working_points.rend(); ++wp) {
+            if(muon.GetIsolation() < wp->second) {
+                region.SetLowerIso(wp->first);
                 if(wp != working_points.rbegin())
-                    region.SetUpperIso(*(--wp));
+                    region.SetUpperIso((--wp)->first);
                 break;
             }
         }
 
+        if(muon.GetIsolation() > 0.15 && muon.GetIsolation() < 0.3) return EventRegion::Unknown();
         return region;
     }
 };
