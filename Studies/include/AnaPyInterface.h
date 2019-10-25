76daf4e736a803c8245e510231f0a11e86b2dbc3
MRD2F <33909461+MRD2F@users.noreply.github.com>
2019-10-25 23:15:50 +0200

    Implementation of new signal selection model  (#120)
    * new Signal Selection ML implementation
    
    *  Applied PR comments and create Optimizar.py, CalculateWeight and ParamsModel
    
    * - Applied comments PR
    - Update configs about for normalize and scale layers
    - Add configs for the x-sec and br of the res signal samples
    
    * Delete mean_std.json
    
    * applied comments and change name of ParamsModel->ParametrizedModel
    
    * couple of fixes
    
    *  update file name in Optimizer.py

---
+++ Studies/include/AnaPyInterface.h
@@ -0,0 +1,181 @@
+/*! Definition of functions for calulating most used quantities
+This file is part of https://github.com/hh-italian-group/hh-bbtautau. */
+#pragma once
+
+using LorentzVectorM = ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> >;
+using LorentzVectorE = ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<float> >;
+using LorentzVector = ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >;
+
+LorentzVectorM getHTTp4 (const ROOT::VecOps::RVec<LorentzVectorM>& lep_p4, const ROOT::VecOps::RVec<int>& lep_genTauIndex)
+{
+    size_t n_tau = 0;
+    LorentzVector htt(0, 0, 0, 0);
+    for(size_t n = 0; n < lep_p4.size(); ++n) {
+        if(lep_genTauIndex.at(n) >= 0) {
+            htt += lep_p4.at(n);
+            n_tau++;
+        }
+    }
+    if(n_tau != 2)
+        throw std::runtime_error("too few taus");
+    return LorentzVectorM(htt);
+}
+
+float HTTScalarPt (const ROOT::VecOps::RVec<LorentzVectorM>& lep_p4, const ROOT::VecOps::RVec<int>& lep_genTauIndex)
+{
+    size_t n_tau = 0;
+    float h_tautau_pt_scalar = 0.f;
+    for(size_t n = 0; n < lep_p4.size(); ++n) {
+        if(lep_genTauIndex.at(n) >= 0) {
+            h_tautau_pt_scalar += lep_p4.at(n).Pt();
+            n_tau++;
+        }
+    }
+    if(n_tau != 2)
+        throw std::runtime_error("too few taus");
+
+    return h_tautau_pt_scalar;
+}
+
+ROOT::VecOps::RVec<float> MakeDeepFlavour_bVSall (const ROOT::VecOps::RVec<float>& jets_deepFlavour_b,
+                                                  const ROOT::VecOps::RVec<float>& jets_deepFlavour_bb,
+                                                  const ROOT::VecOps::RVec<float>& jets_deepFlavour_lepb)
+{
+    ROOT::VecOps::RVec<float> b_vs_all(jets_deepFlavour_b.size());
+    for(size_t n = 0; n < jets_deepFlavour_b.size(); ++n)
+        b_vs_all.at(n) = jets_deepFlavour_b.at(n) + jets_deepFlavour_bb.at(n) + jets_deepFlavour_lepb.at(n);
+    return b_vs_all;
+}
+
+float jets_deepFlavour (const ROOT::VecOps::RVec<float>& deepFlavour_bVSall,
+                        const ROOT::VecOps::RVec<size_t>& df_index, const size_t n)
+{
+    if(n < df_index.size()){
+        size_t selected_index = df_index.at(n);
+        return deepFlavour_bVSall.at(selected_index);
+    }
+    else
+        return 0;
+}
+
+float jets_deepCSV (const ROOT::VecOps::RVec<float>& deepCsv_BvsAll,
+                    const ROOT::VecOps::RVec<size_t>& df_index, const size_t n)
+{
+    if(n < df_index.size()){
+        size_t selected_index = df_index.at(n);
+        return deepCsv_BvsAll.at(selected_index);
+    }
+    else
+        return 0;
+}
+
+
+float jet_p4_pt (const ROOT::VecOps::RVec<size_t>& df_index,
+                 const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, const size_t n)
+{   if(n < df_index.size())
+        return jets_p4.at(df_index.at(n)).pt();
+    else
+        return 0;
+}
+
+float jet_p4_eta (const ROOT::VecOps::RVec<size_t>& df_index,
+                  const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, const size_t n)
+{   if(n < df_index.size())
+        return jets_p4.at(df_index.at(n)).eta();
+    else
+        return 0;
+}
+
+float jet_p4_E (const ROOT::VecOps::RVec<size_t>& df_index,
+                const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, const size_t n)
+{   if(n < df_index.size())
+        return jets_p4.at(df_index.at(n)).E();
+    else
+        return 0;
+}
+
+float jet_p4_M (const ROOT::VecOps::RVec<size_t>& df_index,
+                const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, const size_t n)
+{   if(n < df_index.size())
+        return jets_p4.at(df_index.at(n)).M();
+    else
+        return 0;
+}
+
+float rel_jet_M_pt (const ROOT::VecOps::RVec<size_t>& df_index,
+                    const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, const size_t n)
+{   if(n < df_index.size())
+        return jets_p4.at(df_index.at(n)).M() / jets_p4.at(df_index.at(n)).Pt();
+    else
+        return 0;
+}
+
+float rel_jet_E_pt (const ROOT::VecOps::RVec<size_t>& df_index,
+                    const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, const size_t n)
+{   if(n < df_index.size())
+        return jets_p4.at(df_index.at(n)).E() / jets_p4.at(df_index.at(n)).Pt();
+    else
+        return 0;
+}
+
+
+int jet_genbJet (const ROOT::VecOps::RVec<int>& jet_genJetIndex,
+                 const ROOT::VecOps::RVec<size_t>& ordered_jet_indexes, const size_t n,
+                 const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4)
+{
+    if(n < ordered_jet_indexes.size()){
+        size_t index = ordered_jet_indexes.at(n);
+        return jet_genJetIndex.at(index) >= 0;
+    }
+    else
+        return 0;
+}
+
+ROOT::VecOps::RVec<int> MakeGenbJet (const ROOT::VecOps::RVec<int>& jet_genJetIndex,
+                                     const ROOT::VecOps::RVec<size_t>& ordered_jet_indexes)
+{
+    ROOT::VecOps::RVec<int> jets_genbJet(ordered_jet_indexes.size());
+    for(size_t n = 0; n < ordered_jet_indexes.size(); ++n){
+        const size_t index = ordered_jet_indexes.at(n);
+        jets_genbJet.at(n) = jet_genJetIndex.at(index) >= 0;
+    }
+    return jets_genbJet;
+}
+
+ROOT::VecOps::RVec<size_t> CreateOrderedIndex (const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4,
+                                               const ROOT::VecOps::RVec<float>& jets_deepFlavour,
+                                               bool apply_acceptance, size_t max_jet = std::numeric_limits<size_t>::max())
+{
+    ROOT::VecOps::RVec<size_t> ordered_index;
+    for(size_t jet_index = 0; jet_index < jets_p4.size(); ++jet_index){
+            if(apply_acceptance && (jets_p4.at(jet_index).pt() < 20 || abs(jets_p4.at(jet_index).eta()) > 2.4)) continue;
+            ordered_index.push_back(jet_index);
+            if(ordered_index.size() == max_jet)
+                break;
+
+    }
+
+    std::sort(ordered_index.begin(), ordered_index.end(), [&](size_t a, size_t b){
+        return jets_deepFlavour.at(a) > jets_deepFlavour.at(b);
+    });
+
+    return ordered_index;
+}
+
+float httDeltaPhi_jet (const LorentzVectorM& htt_p4,const ROOT::VecOps::RVec<size_t>& df_index,
+                       const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, const size_t n)
+{
+    if(n < df_index.size())
+        return ROOT::Math::VectorUtil::DeltaPhi(htt_p4, jets_p4.at(df_index.at(n)));
+    else
+        return 0;
+}
+
+float httDeltaEta_jet (const LorentzVectorM& htt_p4, const ROOT::VecOps::RVec<size_t>& df_index,
+                       const ROOT::VecOps::RVec<LorentzVectorE>& jets_p4, size_t n)
+{
+    if(n < df_index.size())
+        return (htt_p4.eta() - jets_p4.at(df_index.at(n)).eta());
+    else
+        return 0;
+}
---
+++ Studies/python/CalculateWeigths.py
@@ -0,0 +1,204 @@
+import sys
+import pandas as pd
+import numpy as np
+import numba
+from scipy import interpolate
+
+from InputsProducer import SampleType as st
+
+ggHH_NonRes = st.ggHH_NonRes -7
+VBFHH_NonRes = st.VBFHH_NonRes -7
+ggHH_Res = st.ggHH_Res -7
+VBFHH_Res = st.VBFHH_Res -7
+
+@numba.njit
+def calculeteNEvents(X, Z):
+    mass_node = np.where(Z[:, 0, 3] == -1, Z[:, 0, 2], Z[:, 0, 3])
+    mass_node_unique = np.unique(mass_node)
+    counts = np.zeros((3,3,4,2,len(mass_node_unique)))
+
+    for n in range(X.shape[0]):
+        year = int(X[n, 0, 8])#var_pos['sample_year']]
+        channel = int(X[n, 0, 9])#var_pos['channelId']]
+        sample_type = int(Z[n,0,0])
+        spin = int(Z[n,0, 1] > 0)
+        mn = mass_node[n]
+        mn_index = np.argmax(mass_node_unique == mn)
+
+        counts[year - 2016, channel, sample_type - 6, spin,  mn_index] +=1
+    weights = np.where(counts > 0, 1. / counts, 0)
+    return counts, weights, mass_node_unique, mass_node
+
+def CreateXSTable(X, Z, radion_xs, radion_br, vbf_radion_xs, graviton_xs, graviton_br, vbf_graviton_xs):
+
+    counts, weights, mass_node_unique, mass_node = calculeteNEvents(X, Z)
+
+    df_xs_radion = pd.read_table(radion_xs)
+    df_xs_vbf_radion = pd.read_table(vbf_radion_xs)
+    df_br_radion = pd.read_table(radion_br)
+
+    df_xs_graviton = pd.read_table(graviton_xs)
+    df_xs_vbf_graviton = pd.read_table(vbf_graviton_xs)
+    df_br_graviton = pd.read_table(graviton_br)
+
+    f_br_radion = interpolate.interp1d(df_br_radion['mR(GeV)'], df_br_radion['hh'], kind='cubic')
+    f_xs_radion = interpolate.interp1d(df_xs_radion['mH(GeV)'], df_xs_radion['CrossSection(pb)'], kind='cubic')
+    f_xs_vbf_radion = interpolate.interp1d(df_xs_vbf_radion['mH(GeV)'], df_xs_vbf_radion['CrossSection(pb)'], kind='cubic')
+
+    f_br_graviton = interpolate.interp1d(df_br_graviton['mG(GeV)'], df_br_graviton['hh'], kind='cubic')
+    f_xs_graviton = interpolate.interp1d(df_xs_graviton['mG(GeV)'], df_xs_graviton['XS(pb)'], kind='cubic')
+    f_xs_vbf_graviton = interpolate.interp1d(df_xs_vbf_graviton['mG(GeV)'], df_xs_vbf_graviton['XS(pb)'], kind='cubic')
+
+    xs_all = np.ones((4,2,len(mass_node_unique)))
+    indices = mass_node_unique >= 250
+    res_masses = np.copy(mass_node_unique[indices])
+    if res_masses[0] == 250 :
+        res_masses[0] = 251
+
+    xs_radion = f_xs_radion(res_masses)
+    xs_vbf_radion = f_xs_vbf_radion(res_masses)
+    br_radion = f_br_radion(res_masses)
+
+    xs_graviton = f_xs_graviton(res_masses)
+    xs_vbf_graviton = f_xs_vbf_graviton(res_masses)
+    br_graviton = f_br_graviton(res_masses)
+
+    xs_all[ggHH_Res,0, indices] = xs_radion * br_radion
+    xs_all[VBFHH_Res,0, indices] = xs_vbf_radion * br_radion
+
+    xs_all[ggHH_Res,1, indices] = xs_graviton * br_graviton
+    xs_all[VBFHH_Res,1, indices] = xs_vbf_graviton * br_graviton
+
+    return xs_all, counts, weights, mass_node_unique, mass_node
+
+
+def CreateWeights(X, Z):
+    xs_all, counts, weights, mass_node_unique, mass_node = CreateXSTable(X, Z, '../config/xs_br/radion_ggF_xs.csv',
+                                                                        '../config/xs_br/radion_br.csv',
+                                                                        '../config/xs_br/radion_VBF_xs.csv',
+                                                                        '../config/xs_br/graviton_ggF_xs.csv',
+                                                                        '../config/xs_br/graviton_br.csv',
+                                                                        '../config/xs_br/graviton_VBF_xs.csv')
+
+    for sample_type in range(4):
+        for spin in range(2):
+            for mass in range(xs_all.shape[2]):
+                weights[:,:,sample_type,spin,mass] *= xs_all[sample_type, spin, mass]
+
+    # Radion = Graviton
+    indices = mass_node_unique >= 250
+    res_process = [ggHH_Res, VBFHH_Res]
+    non_res_process = [ggHH_NonRes, VBFHH_NonRes]
+    all_process = [res_process, non_res_process]
+    for year in range(3):
+        for channel in range(3):
+            for prod_mode in res_process:
+                radion = np.sum(weights[year, channel, prod_mode, 0, :] * counts[year, channel, prod_mode, 0, :])
+                graviton = np.sum(weights[year, channel, prod_mode, 1, :] * counts[year, channel, prod_mode, 1, :])
+                if radion == 0 or graviton == 0: continue
+                C = radion / graviton
+                weights[year, channel, prod_mode, 1, :] *= C
+
+    # VBF = ggF
+    for year in range(3):
+        for channel in range(3):
+            for x in all_process:
+                VBF = np.sum(weights[year, channel, x[1], :, :] * counts[year, channel, x[1], :, :])
+                ggF = np.sum(weights[year, channel,  x[0], :, :] * counts[year, channel,  x[0], :, :])
+                if VBF == 0 or ggF == 0: continue
+                E = VBF / ggF
+                weights[year, channel, x[0], :, :] *= E
+
+    # Res = Non Res
+    for year in range(3):
+        for channel in range(3):
+            non_res = np.sum(weights[year, channel, non_res_process, :, :] * counts[year, channel, non_res_process, :, :])
+            res = np.sum(weights[year, channel, res_process, :, :] * counts[year, channel, res_process, :, :])
+            if non_res == 0 or res == 0: continue
+            D = non_res / res
+            weights[year, channel, res_process, :, :] *= D
+
+    # eTau = muTau = tauTau
+    for year in range(3):
+        ch_cnt = np.zeros(3)
+        for channel in range(3):
+            ch_cnt[channel] = np.sum(weights[year, channel, :, :, :] * counts[year, channel, :, :, :])
+
+        ref_ch = np.amax(ch_cnt)
+        if ref_ch == 0: continue
+
+        for channel in range(3):
+            if ch_cnt[channel] != 0 :
+                A = ref_ch / ch_cnt[channel]
+                weights[year, channel, :, :, :] *= A
+
+    # all years
+    year_cnt = np.zeros(3)
+    for year in range(3):
+         year_cnt[year] = np.sum(weights[year, :, :, :, :] * counts[year, :, :, :, :])
+
+    ref_year = np.amax(year_cnt)
+    if ref_year == 0:
+        raise RuntimeError('bad year')
+
+    for year in range(3):
+        if year_cnt[year] != 0 :
+            A = ref_year / year_cnt[year]
+            weights[year, :, :, :, :] *= A
+
+    #sum all = n_evt
+    weights_count = np.sum(weights * counts)
+    C =  X.shape[0] / weights_count
+
+    weights *= C
+
+    return weights, mass_node_unique, mass_node
+
+@numba.njit
+def ConvertToVector(X,Z, weights, mass_node_unique, mass_node):
+    weight_vec = np.zeros(X.shape[0])
+    for n in range(X.shape[0]):
+        year = int(X[n, 0, 8])
+        channel = int(X[n, 0, 9])
+        sample_type = int(Z[n,0,0])
+        spin = int(Z[n,0, 1] > 0)
+        mn = mass_node[n]
+        mn_index = np.argmax(mass_node_unique == mn)
+
+        weight_vec[n] = weights[year - 2016, channel, sample_type - 6, spin,  mn_index]
+    return weight_vec
+
+def CreateSampleWeigts(X,Z):
+    weights, mass_node_unique, mass_node = CreateWeights(X, Z)
+    return ConvertToVector(X,Z, weights, mass_node_unique, mass_node)
+
+@numba.njit
+def CrossCheckWeights(Z, X, w, g_r, res_non_res, check_channel, year, channel):
+    w = ConvertToVector(X, Z)
+    w_1 = []
+    w_2 = []
+    w_3 = []
+    if res_non_res == True:
+        for n in range(X.shape[0]):
+            if (Z[n, 0, 0] == 6 or Z[n, 0, 0] == 7) and X[n, 0, 8] == year :
+                w_1.append(w[n])
+            elif (Z[n, 0, 0] == 8 or Z[n, 0, 0] == 9) and X[n, 0, 8] == year :
+                w_2.append(w[n])
+
+    elif g_r == True:
+        for n in range(X.shape[0]):
+            if Z[n, 0, 1] == 0 and X[n, 0, 8] == year:
+                w_1.append(w[n])
+            elif Z[n, 0, 1] == 2 and X[n, 0, 8] == year:
+                w_2.append(w[n])
+
+    elif check_channel == True:
+        for n in range(X.shape[0]):
+            if X[n, 0, 9] == 0 and X[n, 0, 8] == year:
+                w_1.append(w[n])
+            elif X[n, 0, 9] == 1 and X[n, 0, 8] == year:
+                w_2.append(w[n])
+            elif X[n, 0, 9] == 2 and X[n, 0, 8] == year:
+                w_3.append(w[n])
+
+    return np.sum(np.array(w_1)), np.sum(np.array(w_2)), np.sum(np.array(w_3))
