8bb816e4dbd1d0b2dea41e386018c46341e4d363
Maria Rosaria Di Domenico <maria.rosaria.di.domenico@cern.ch>
2020-05-27 13:14:37 +0200

    fix

--- Analysis/src/BaseEventAnalyzer.cpp
+++ Analysis/src/BaseEventAnalyzer.cpp
@@ -273,8 +273,10 @@ void BaseEventAnalyzer::ProcessDataSource(const SampleDescriptor& sample, const 
                 if(ana_setup.period == Period::Run2016 || ana_setup.period == Period::Run2017)
                     l1_prefiring_weight = (*event)->l1_prefiring_weight;
 
-                jet_pu_id_weight = eventWeights_HH->GetProviderT<mc_corrections::JetPuIdWeights>(
+                auto jet_pu_id_weight_provided = eventWeights_HH->GetProviderT<mc_corrections::JetPuIdWeights>(
                         mc_corrections::WeightType::JetPuIdWeights);
+                jet_pu_id_weight = jet_pu_id_weight_provided->Get(*event);
+
             }
             const auto eventCategories = DetermineEventCategories(*event, pass_vbf_trigger);
             for(auto eventCategory : eventCategories) {
