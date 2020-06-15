b31b903e3189edfe2c36e95898ac8df49c6c95b2
Marcel R <github.riga@icloud.com>
2020-06-15 13:11:09 +0200

    Add running inference test.

--- Analysis/source/CalcMulticlassDNN.cxx
+++ Analysis/source/CalcMulticlassDNN.cxx
@@ -23,24 +23,42 @@ public:
         outputFile_(root_ext::CreateRootFile(args_.output())) {}
 
   void Run() {
-    // test
-    std::cout << args_.channel() << std::endl;
-    std::cout << args_.period() << std::endl;
-    std::cout << args_.input() << std::endl;
-    std::cout << args_.output() << std::endl;
-
-    std::string channelName = EnumNameMap<Channel>::GetDefault().EnumToString(args_.channel());
+    std::cout << "running multiclass classification inference" << std::endl;
+    std::cout << "channel: " << args_.channel() << std::endl;
+    std::cout << "period : " << args_.period() << std::endl;
+    std::cout << "input  : " << args_.input() << std::endl;
+    std::cout << "output : " << args_.output() << std::endl;
 
     // open the input tree
+    std::string channelName = EnumNameMap<Channel>::GetDefault().EnumToString(args_.channel());
     TTree* inTree = (TTree*)inputFile_->Get(channelName.c_str());
 
     // create the output tree
     outputFile_->cd();
     TTree* outTree = new TTree(channelName.c_str(), channelName.c_str());
 
+    // TODO: setup and run inference here
+    // test
+    hmc::Model* model = hmc::loadModel(2018, "v0", "kl1_c2v1_c31");
+    model->input.clear();
+    for (int f = 0; f < (int)model->getNumberOfFeatures(); f++) {
+        std::string featureName = model->getFeatureName(f);
+        model->input.setValue(featureName, (float)f);
+    }
+    model->run(12);
+    for (const auto& it : model->output) {
+        std::cout << "Node " << it.first << ", value " << it.second << std::endl;
+    }
+    // test end
+
     // write the output file
+    std::cout << "writing output tree" << std::endl;
     outputFile_->Write();
+
+    // close files and finish
     outputFile_->Close();
+    inputFile_->Close();
+    std::cout << "done" << std::endl;
   }
 
 private:
