23cf42463ee16eae5ff1bae7e3a7fa077620b4ca
Marcel R <github.riga@icloud.com>
2020-06-16 16:39:58 +0200

    Start implementing features.

--- Analysis/source/CalcMulticlassDNN.cxx
+++ Analysis/source/CalcMulticlassDNN.cxx
@@ -20,25 +20,11 @@ struct CalcMulticlassDNNArguments {
 class FeatureProvider {
 
   public:
-  FeatureProvider(TTree* inTree)
-      : inTree_(inTree) {
-    // define variables to read
-    ulong64Inputs_.emplace("evt", 0);
-    floatInputs_.emplace("tau1_pt", 0.);
-
-    // set branch adresses
-    for (auto& it : floatInputs_) {
-      inTree->SetBranchAddress(it.first.c_str(), &it.second);
-    }
-    for (auto& it : boolInputs_) {
-      inTree->SetBranchAddress(it.first.c_str(), &it.second);
-    }
-    for (auto& it : ulong64Inputs_) {
-      inTree->SetBranchAddress(it.first.c_str(), &it.second);
-    }
-  }
+  FeatureProvider(Period period, Channel channel, TTree* inTree);
 
   FeatureProvider(const FeatureProvider&) = delete;
+
+  void calculate();
 
   inline hmc::EventId getEventId() const {
     return hmc::EventId(ulong64Inputs_.at("evt"));
@@ -48,16 +34,7 @@ class FeatureProvider {
     features_.emplace(featureName, 0.);
   }
 
-  void calculate(int i) {
-    inTree_->GetEntry(i);
-
-    for (auto& it : features_) {
-      // TODO
-      it.second = 0.5;
-    }
-  }
-
-  float get(const std::string& featureName) const {
+  inline float get(const std::string& featureName) const {
     const auto& it = features_.find(featureName);
     if (it == features_.end()) {
       throw exception("FeatureProvider: unknown feature '" + featureName + "'");
@@ -66,7 +43,8 @@ class FeatureProvider {
   }
 
   private:
-  TTree* inTree_;
+  Period period_;
+  Channel channel_;
   std::map<std::string, float> floatInputs_;
   std::map<std::string, bool> boolInputs_;
   std::map<std::string, ULong64_t> ulong64Inputs_;
@@ -83,10 +61,12 @@ class CalcMulticlassDNN {
 
   void Run() {
     std::cout << "running multiclass classification inference" << std::endl;
-    std::cout << "channel: " << args_.channel() << std::endl;
-    std::cout << "period : " << args_.period() << std::endl;
-    std::cout << "input  : " << args_.input() << std::endl;
-    std::cout << "output : " << args_.output() << std::endl;
+    std::cout << "channel : " << args_.channel() << std::endl;
+    std::cout << "period  : " << args_.period() << std::endl;
+    std::cout << "input   : " << args_.input() << std::endl;
+    std::cout << "output  : " << args_.output() << std::endl;
+    std::cout << "end     : " << args_.end() << std::endl;
+    std::cout << "progress: " << args_.progress() << std::endl;
 
     // disable potential multi-threading to preserve the order of entries
     ROOT::DisableImplicitMT();
@@ -96,7 +76,7 @@ class CalcMulticlassDNN {
     TTree* inTree = (TTree*)inputFile_->Get(channelName.c_str());
 
     // create the input feature provider
-    FeatureProvider features(inTree);
+    FeatureProvider features(args_.period(), args_.channel(), inTree);
 
     // create the output tree
     outputFile_->cd();
@@ -113,7 +93,7 @@ class CalcMulticlassDNN {
       models.push_back(hmc::loadModel(int(args_.period()), version, tag));
       hmc::Model* model = models.back();
 
-      // propagate required features to the feature provider
+      // register required features with the feature provider
       for (const auto& featureName : model->getFeatureNames()) {
         features.add(featureName);
       }
@@ -129,8 +109,9 @@ class CalcMulticlassDNN {
     // start iterating
     int nEntries = args_.end() > 0 ? args_.end() : inTree->GetEntries();
     for (int i = 0; i < nEntries; i++) {
-      // calculate features for this entry
-      features.calculate(i);
+      // load the entry and calculate features
+      inTree->GetEntry(i);
+      features.calculate();
 
       // fill features of all models and run them
       for (hmc::Model*& model : models) {
@@ -166,6 +147,178 @@ class CalcMulticlassDNN {
   std::shared_ptr<TFile> inputFile_;
   std::shared_ptr<TFile> outputFile_;
 };
+
+FeatureProvider::FeatureProvider(Period period, Channel channel, TTree* inTree)
+    : period_(period)
+    , channel_(channel) {
+  // define names of variables to read
+  // TODO
+  std::vector<std::string> ulong64Names = { "evt" };
+  std::vector<std::string> floatNames = { "tau1_pt" };
+  std::vector<std::string> boolNames = {};
+
+  // register them in inputs and set branch addresses
+  for (const auto& name : ulong64Names) {
+    ulong64Inputs_.emplace(name, 0);
+    inTree->SetBranchAddress(name.c_str(), &ulong64Inputs_.at(name));
+  }
+  for (const auto& name : floatNames) {
+    floatInputs_.emplace(name, 0.);
+    inTree->SetBranchAddress(name.c_str(), &floatInputs_.at(name));
+  }
+  for (const auto& name : boolNames) {
+    boolInputs_.emplace(name, 0.);
+    inTree->SetBranchAddress(name.c_str(), &boolInputs_.at(name));
+  }
+}
+
+void FeatureProvider::calculate() {
+  for (auto& it : features_) {
+    if (it.first == "is_mutau") {
+      it.second = float(channel_ == Channel::MuTau);
+    } else if (it.first == "is_etau") {
+      it.second = float(channel_ == Channel::ETau);
+    } else if (it.first == "is_tautau") {
+      it.second = float(channel_ == Channel::TauTau);
+    } else if (it.first == "n_jets_20") {
+      it.second = 0.5;
+    } else if (it.first == "n_bjets_20") {
+      it.second = 0.5;
+    } else if (it.first == "is_vbf_loose_cat") {
+      it.second = 0.5;
+    } else if (it.first == "is_vbf_tight_cat") {
+      it.second = 0.5;
+    } else if (it.first == "is_resolved_1b_cat") {
+      it.second = 0.5;
+    } else if (it.first == "is_resolved_2b_cat") {
+      it.second = 0.5;
+    } else if (it.first == "is_boosted_cat") {
+      it.second = 0.5;
+    } else if (it.first == "bjet1_pt") {
+      it.second = 0.5;
+    } else if (it.first == "bjet1_eta") {
+      it.second = 0.5;
+    } else if (it.first == "bjet1_phi") {
+      it.second = 0.5;
+    } else if (it.first == "bjet1_e") {
+      it.second = 0.5;
+    } else if (it.first == "bjet1_deepflavor_b") {
+      it.second = 0.5;
+    } else if (it.first == "bjet1_deepflavor_c") {
+      it.second = 0.5;
+    } else if (it.first == "bjet2_pt") {
+      it.second = 0.5;
+    } else if (it.first == "bjet2_eta") {
+      it.second = 0.5;
+    } else if (it.first == "bjet2_phi") {
+      it.second = 0.5;
+    } else if (it.first == "bjet2_e") {
+      it.second = 0.5;
+    } else if (it.first == "bjet2_deepflavor_b") {
+      it.second = 0.5;
+    } else if (it.first == "bjet2_deepflavor_c") {
+      it.second = 0.5;
+    } else if (it.first == "jet3_pt") {
+      it.second = 0.5;
+    } else if (it.first == "jet3_eta") {
+      it.second = 0.5;
+    } else if (it.first == "jet3_phi") {
+      it.second = 0.5;
+    } else if (it.first == "jet3_e") {
+      it.second = 0.5;
+    } else if (it.first == "jet3_deepflavor_b") {
+      it.second = 0.5;
+    } else if (it.first == "jet3_deepflavor_c") {
+      it.second = 0.5;
+    } else if (it.first == "jet4_pt") {
+      it.second = 0.5;
+    } else if (it.first == "jet4_eta") {
+      it.second = 0.5;
+    } else if (it.first == "jet4_phi") {
+      it.second = 0.5;
+    } else if (it.first == "jet4_e") {
+      it.second = 0.5;
+    } else if (it.first == "jet4_deepflavor_b") {
+      it.second = 0.5;
+    } else if (it.first == "jet4_deepflavor_c") {
+      it.second = 0.5;
+    } else if (it.first == "jet5_pt") {
+      it.second = 0.5;
+    } else if (it.first == "jet5_eta") {
+      it.second = 0.5;
+    } else if (it.first == "jet5_phi") {
+      it.second = 0.5;
+    } else if (it.first == "jet5_e") {
+      it.second = 0.5;
+    } else if (it.first == "jet5_deepflavor_b") {
+      it.second = 0.5;
+    } else if (it.first == "jet5_deepflavor_c") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet1_pt") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet1_eta") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet1_phi") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet1_e") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet1_deepflavor_b") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet1_deepflavor_c") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet2_pt") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet2_eta") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet2_phi") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet2_e") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet2_deepflavor_b") {
+      it.second = 0.5;
+    } else if (it.first == "vbfjet2_deepflavor_c") {
+      it.second = 0.5;
+    } else if (it.first == "lep1_pt") {
+      it.second = 0.5;
+    } else if (it.first == "lep1_eta") {
+      it.second = 0.5;
+    } else if (it.first == "lep1_phi") {
+      it.second = 0.5;
+    } else if (it.first == "lep1_e") {
+      it.second = 0.5;
+    } else if (it.first == "lep2_pt") {
+      it.second = 0.5;
+    } else if (it.first == "lep2_eta") {
+      it.second = 0.5;
+    } else if (it.first == "lep2_phi") {
+      it.second = 0.5;
+    } else if (it.first == "lep2_e") {
+      it.second = 0.5;
+    } else if (it.first == "met_pt") {
+      it.second = 0.5;
+    } else if (it.first == "met_phi") {
+      it.second = 0.5;
+    } else if (it.first == "bh_pt") {
+      it.second = 0.5;
+    } else if (it.first == "bh_eta") {
+      it.second = 0.5;
+    } else if (it.first == "bh_phi") {
+      it.second = 0.5;
+    } else if (it.first == "bh_e") {
+      it.second = 0.5;
+    } else if (it.first == "tauh_sv_pt") {
+      it.second = 0.5;
+    } else if (it.first == "tauh_sv_eta") {
+      it.second = 0.5;
+    } else if (it.first == "tauh_sv_phi") {
+      it.second = 0.5;
+    } else if (it.first == "tauh_sv_e") {
+      it.second = 0.5;
+    } else {
+      throw exception("MulticlassInference: undhandled feature '" + it.first + "'");
+    }
+  }
+}
 
 } // namespace analysis
 
